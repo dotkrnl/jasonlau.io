2e2d1d891f0769f8fb54bf94234d8bfcc170929f
Jason Lau <i@dotkrnl.com>
2026-03-03 17:43:53 -0800

    feat: add Tailwind CLI build hook and HTML minification in Eleventy

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+const { execSync } = require("child_process");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/*.png");
   eleventyConfig.addPassthroughCopy("src/*.ico");
@@ -5,6 +7,29 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/profile-picture.png");
   eleventyConfig.addPassthroughCopy("src/design-works");
   eleventyConfig.addPassthroughCopy("src/research-papers");
+
+  // Build Tailwind CSS after Eleventy generates HTML (so it scans final output)
+  eleventyConfig.on("eleventy.after", () => {
+    execSync(
+      "npx @tailwindcss/cli -i src/styles.css -o _site/styles.css --minify",
+      { stdio: "inherit" }
+    );
+  });
+
+  // Minify HTML output
+  eleventyConfig.addTransform("htmlmin", async function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      const { minify } = await import("html-minifier-terser");
+      return minify(content, {
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+      });
+    }
+    return content;
+  });
 
   return {
     dir: {
